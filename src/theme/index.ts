810f4ada6effa5799b04c4c0b3f1058b3a031054
samlin <zhlin1122@gmail.com>
2023-04-30 17:12:50 +0800

    chore: update theme

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,28 +1,40 @@
 import { createTheme } from "@mui/material/styles";
-import { red } from "@mui/material/colors";
 
 // Create a theme instance.
 const theme = createTheme({
   palette: {
+    common: {
+      black: "#020202"
+    },
     primary: {
-      main: "#556cd6"
+      main: "#F7C324",
+      contrastText: "#020202"
     },
     secondary: {
-      main: "#19857b"
+      main: "#020202",
+      contrastText: "#ffffff"
     },
-    error: {
-      main: red.A400
+    text: {
+      primary: "#020202",
+      disabled: "#D1D1D1"
+    },
+    divider: "#D1D1D1",
+    background: {
+      paper: "#F8F8F8"
     }
   },
   typography: {
     fontSize: 16,
     fontFamily: ["Noto Sans TC", "Inter", " system-ui", "Avenir", "Helvetica", "Arial", "sans-serif"].join(","),
     h1: { fontSize: "3rem" },
-    h2: { fontSize: "2.5rem" },
-    h3: { fontSize: "2rem" },
+    h2: { fontSize: "2.5rem" }, // 40px
+    h3: { fontSize: "2rem" }, // 32px
     h4: { fontSize: "1.75rem" },
-    h5: { fontSize: "1.5rem" },
-    h6: { fontSize: "1.25rem" }
+    h5: { fontSize: "1.5rem" }, // 24px
+    h6: { fontSize: "1.25rem" }, // 20px
+    button: {
+      fontSize: "20px"
+    }
   }
 });
 
